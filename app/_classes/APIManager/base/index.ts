8494c01deda56100a5586c7dd11cc535dc0713d3
Lucas-Christian <lucas.christian.programmer@gmail.com>
2023-12-12 16:12:33 -0300

    feat: login and register finished

---
+++ app/_classes/APIManager/base/index.ts
@@ -0,0 +1,42 @@
+import type { BackendRoutes } from "./types/BackendRoutes.types";
+import { CookieManager } from "@classes/CookieManager";
+
+export class APIManager {
+  protected static async request<U extends BackendRoutes>(
+    url: U,
+    body: string | FormData,
+    headers: HeadersInit = {},
+    useServer: { useServer: boolean },
+    method: string = "POST"
+  ): Promise<Response> {
+    const response = await fetch(`${process.env["NEXT_PUBLIC_BACKEND_URL"]}${url}`, {
+      method: method,
+      headers: headers,
+      body: body,
+    });
+
+    const authorization = response.headers.get("Authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+      const accessToken = authorization.substring(7);
+      CookieManager.set(accessToken, useServer);
+    }
+
+    return response;
+  }
+
+  protected static async handleResponse<T>(response: Response, useServer: { useServer: boolean }): Promise<void | T | { error: string; message: string; }> {
+    if (response.status === 401) {
+      this.signOut(useServer);
+      if (window) window.location.href = "/auth/signin";
+      return;
+    }
+    const data = await response.json();
+    if (data["errors"]) return { error: data["error"] as string, message: data["message"] as string};
+    return data;
+  }
+
+  public static async signOut({ useServer }: { useServer: boolean } = { useServer: false }): Promise<void> {
+    const accessToken = CookieManager.get({ useServer });
+    if (accessToken) CookieManager.delete({ useServer });
+  }
+}
--- app/_components/Input/index.tsx
+++ app/_components/Input/index.tsx
@@ -2,9 +2,9 @@ import styles from "./Input.module.css";
 import React, { HTMLAttributes } from "react";
 
 type InputProps = {
-    placeholder?: string;
-    name?: string;
-    type: "text" | "email" | "password" | "checkbox";
+  placeholder?: string;
+  name?: string;
+  type: "text" | "email" | "password" | "checkbox";
 } & HTMLAttributes<HTMLInputElement>;
 
 export const Input = ({ type, name, placeholder, ...props }: InputProps) => {
